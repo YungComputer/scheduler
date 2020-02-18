b217aaf02c915545e7cd4c947c939402914b27b6
Clare <56135752+YungComputer@users.noreply.github.com>
2020-02-17 19:53:54 -0800

    add reset and cancel functions

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
+import { tsPropertySignature } from "@babel/types";
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  function reset() {
+    setName("");
+    setInterviewer(null);
+  } 
+  function cancel() {
+    reset()
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -27,7 +36,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
+          <Button onClick={cancel} danger>Cancel</Button>
           <Button confirm>Save</Button>
         </section>
       </section>
