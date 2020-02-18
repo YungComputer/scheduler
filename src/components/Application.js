e421f449354ec89e8eb8adc09c78fb173fb8dafe
Clare <56135752+YungComputer@users.noreply.github.com>
2020-02-18 14:44:40 -0800

    store API days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList.js";
 import Appointment from "components/Appointment/index.js";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
 
 const appointments = [
   {
@@ -71,11 +55,17 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([])
   const schedule = appointments.map((appointment) => {
     return (
       <Appointment key={appointment.id} {...appointment} />
     )
   })
+  useEffect(() => {
+    axios.get('/api/days').then((response) => {
+      setDays(response.data)
+    })
+  }, [day])
   return (
     <main className="layout">
       <section className="sidebar">
