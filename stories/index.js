a086fc98de83258441ae191f51ed8c5dba5d5ad5
Clare <56135752+YungComputer@users.noreply.github.com>
2020-02-17 18:16:05 -0800

    Header component

--- stories/index.js
+++ stories/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
-
 import Button from "components/Button";
 import DayListItem from "components/DayListItem.js";
 import DayList from "components/DayList.js";
 import InterviewerListItem from "components/InterviewerListItem.js";
 import InterviewerList from "components/InterviewerList.js";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -128,3 +128,11 @@ storiesOf("InterviewerList", module)
     onChange={action("onChange")}
   />
 ));
+
+storiesOf("Appointment", module)
+.addParameters({
+  backgrounds: [{ name: "white", value: "#fff", default: true }]
+})
+.add("Appointment", () => <Appointment />)
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time="12pm" />)
