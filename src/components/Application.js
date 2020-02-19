cf00cda3c8f577b226620e0553381f6f3b049f97
Clare <56135752+YungComputer@users.noreply.github.com>
2020-02-19 15:10:50 -0800

    add empty show and create in appointment index

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,56 +6,6 @@ import Appointment from "components/Appointment/index.js";
 import { getAppointmentsForDay, getInterview } from "../helpers/selectors.js"
 
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm"
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png"
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     student: "Clare Nolan",
-//     interviewer: {
-//       id: 2,
-//       name: "Tori Malcolm",
-//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "11am",
-//     student: "James Cohen",
-//     interviewer: {
-//       id: 3,
-//       name: "Mildred Nazir",
-//       avatar: "https://i.imgur.com/T2WwVfS.png"
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "2pm",
-//     student: "Jane Doe",
-//     interviewer: {
-//       id: 4,
-//       name: "Cohana Roy",
-//       avatar: "https://i.imgur.com/FK8V841.jpg"
-//     }
-//   }
-// ];
-
-
 
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
@@ -104,7 +54,6 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {schedule}
-        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,26 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/header.js";
 import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
+import Form from "components/Appointment/Form.js"
+import useVisualMode from "hooks/useVisualMode.js"
 
 export default function Appointment(props) {
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+);
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
+      {mode === CREATE && <Form interviewers={[]} onCancel = {back} />}
+      {mode === SHOW && (
+        <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+        />
       )}
     </article>
   );
