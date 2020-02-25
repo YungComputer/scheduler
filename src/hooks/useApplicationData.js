c248b35be9823baad53dd97a4d94f9b06772b27a
Clare <56135752+YungComputer@users.noreply.github.com>
2020-02-25 15:54:42 -0800

    add in error handling tests for reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,46 +1,13 @@
 import React, { useEffect, useReducer } from "react";
 import axios from "axios";
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-const SET_SPOTS = "SET_SPOTS";
 
-function reducer(state, action) {
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  const SET_SPOTS = "SET_SPOTS";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  SET_SPOTS
+} from "reducers/application.js";
 
-  switch (action.type) {
-    case SET_SPOTS:
-      const indexOfDay = state.days.findIndex(day => day.name === state.day);
-      const newDays = [...state.days];
-      newDays[indexOfDay].spots = newDays[indexOfDay].spots + action.addend;
-      return { ...state, days: newDays };
-    case SET_DAY:
-      return { ...state, day: action.value };
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers,
-        spots: action.spots
-      };
-    case SET_INTERVIEW: {
-      console.log("set_int");
-      return { ...state, appointments: action.value };
-    }
-    case SET_SPOTS: {
-      console.log("before returning in switch statement", action.value);
-      return { ...state, spots: action.value };
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
---
+++ src/reducers/application.test.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  SET_SPOTS
+} from "reducers/application.js";
+
+
+describe("Application Reducer", () => {
+  it("thows an error with an unsupported type", () => {
+    expect(() => reducer({}, { type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+    );
+  });
+});
